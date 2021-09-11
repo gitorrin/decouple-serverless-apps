16ca70372c2301ee6498a752e4fb1cdaec5496e2
EC2 Default User <ec2-user@ip-172-31-34-148.eu-west-1.compute.internal>
2021-09-11 15:00:18 +0000

    Add message attributes

--- exercises/sns/src/publisher/app.js
+++ exercises/sns/src/publisher/app.js
@@ -4,11 +4,22 @@ const sns = new AWS.SNS()
 
 // The Lambda handler
 exports.handler = async (event) => {
+  var type = 'default'
+  if (event.type) {
+    type = event.type
+  }
+  
   // Params object for SNS
   const params = {
     Message: `Message at ${Date()}`,
     Subject: 'New message from publisher',
-    TopicArn: process.env.SNStopic
+    TopicArn: process.env.SNStopic,
+    MessageAttributes: {
+    'notificationType': {
+      DataType: 'String',
+      StringValue: type
+      },
+    },
   }
   
   // Send to SNS
