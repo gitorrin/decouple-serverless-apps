ec0c4756840d0dbf9c75b81437d685a2281a6040
EC2 Default User <ec2-user@ip-172-31-34-148.eu-west-1.compute.internal>
2021-09-15 18:04:40 +0000

    Throw exception from SNS consumer, add lambda destinations

--- exercises/sns/src/consumer/app.js
+++ exercises/sns/src/consumer/app.js
@@ -2,4 +2,5 @@
 // The Lambda handler
 exports.handler = async (event) => {
   console.log(JSON.stringify(event, 2, null))
+  throw new Error('Testing exceptions in lambda consumer of SNS notifications')
 }
